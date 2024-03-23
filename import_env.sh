4b96bce0a3b987c2be98c4808d27215dfd4899d7
junyoung-saltandpepper <junyoung@saltandpepper.kr>
2024-03-23 13:47:13 +0900

    Docs 환경변수 제거

--- import_env.sh
+++ import_env.sh
@@ -1,10 +1,5 @@
 #!/bin/zsh
 
-# 실행환경 USER NAME 설정
-if ! grep -q 'RUNNER_USER_NAME' ~/.zshrc; then
-  echo "export RUNNER_USER_NAME=`whoami`" >> ~/.zshrc
-fi
-
 # 암호화키 담당자 문의
 if ! grep -q 'ENCRYPTION_KEY' ~/.zshrc; then
   echo "export ENCRYPTION_KEY=담당자에게받은암호화키" >> ~/.zshrc
